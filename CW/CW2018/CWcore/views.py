f300366f9da9074070b33df4d08002dd6f9d7902
AIM1993 <244018719@qq.com>
2018-10-15 18:50:58 +0800

    采用Redis对全站进行缓存

--- CW/CW2018/CWcore/views.py
+++ CW/CW2018/CWcore/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.core.cache import cache
+from django.views.decorators.cache import cache_page
+from django_redis import get_redis_connection
 from .models import Author, Article
 
+
+conn = get_redis_connection("default")
 # Create your views here.
+
+@cache_page(60 * 15)
 def home(request):
     if request.method == "GET":
         content = {"article" : Article.objects.all(), "Warning": "For testing"}
         return render(request, "CWcore/home.html", content)
 
 
+@cache_page(60 * 15)
 def about(request):
         if request.method == "GET":
                 return render(request, "CWcore/about.html")
