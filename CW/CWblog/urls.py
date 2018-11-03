39f1d3df9508d5fa86c24a7987d539c1e2ee313f
AIM1993 <244018719@qq.com>
2018-11-03 21:06:58 +0800

    调整前端grid

--- CW/CWblog/urls.py
+++ CW/CWblog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import homeView, blogView, galleryView, aboutView, write_articleView
+from .views import HomeView, BlogView, GalleryView, AboutView, Write_ArticleView, BooksView
 
 app_name = 'CWblog'
 
 urlpatterns = [
-    path('', homeView.as_view(), name="home"),
-    path('blog/', blogView.as_view(), name="blog"),
-    path('gallery/', galleryView.as_view(), name="gallery"),
-    path('about/', aboutView.as_view(), name="about"),
-    path('write_article/', write_articleView.as_view(), name="write_article"),
+    path('', HomeView.as_view(), name="home"),
+    path('blog/', BlogView.as_view(), name="blog"),
+    path('gallery/', GalleryView.as_view(), name="gallery"),
+    path('about/', AboutView.as_view(), name="about"),
+    path('write_Article/', Write_ArticleView.as_view(), name="write_article"),
+    path('booksview/', BooksView.as_view(), name="books"),
 ]
--- CW/CWblog/views.py
+++ CW/CWblog/views.py
@@ -1,39 +1,40 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponseRedirect
-from django.conf import settings
 from django.views import View
-import os
 # from django.core.cache import cache
 # from django.views.decorators.cache import cache_page
 # from django_redis import get_redis_connection
-from .models import Author, Article
-
-
+from .models import Article
 # conn = get_redis_connection("default")
 # Create your views here.
 
 # @cache_page(60)
-class homeView(View):
+class HomeView(View):
     def get(self, request):
         if request.method == "GET":
             content = {"article" : Article.objects.all(), "Warning": "For testing"}
             return render(request, "CWblog/home.html", content)
 
-class blogView(View):
+# @cache_page(60)
+class BlogView(View):
     def get(self, request):
         return render(request, "CWblog/blog.html")
 
 # @cache_page(60)
-class galleryView(View):
+class GalleryView(View):
     def get(self, request):
         return render(request, "CWblog/gallery.html")
 
 # @cache_page(60)
-class aboutView(View):
+class AboutView(View):
     def get(self, request):
         return render(request, "CWblog/about.html")
 
 # @cache_page(60)
-class write_articleView(View):
+class Write_ArticleView(View):
     def get(self, request):
         return render(request, "CWblog/write_article.html")
+
+# @cache_page(60)
+class BooksView(View):
+    def get(self, request):
+        return render(request, "CWblog/books.html")
