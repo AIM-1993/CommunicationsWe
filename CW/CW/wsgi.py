97751f36e8817b63458c4ab3cb54295196abf825
AIM1993 <244018719@qq.com>
2018-11-07 15:47:25 +0800

    修改

--- CW/CW/wsgi.py
+++ CW/CW/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
+# from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
@@ -16,4 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CW.settings')
 
 application = get_wsgi_application()
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
